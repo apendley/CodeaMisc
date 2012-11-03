6cced27ed76d864142289fe4725a16647c79e494
Aaron Pendley <aaronpendley@gmail.com>
2012-11-03 16:39:35 -0400

    - reduced code in importURL

--- ImportURL.lua
+++ ImportURL.lua
@@ -1,32 +1,21 @@
 -- ImportURL
 
 function importURL(...)
-    assert( #arg == 1 or #arg == 2, "Usage: importURL([tabname,] url)" )    
-    
-    local url, tabname
-    
-    if #arg == 1 then
-        url = arg[1]
-    elseif #arg == 2 then
-        tabname = arg[1]
-        url = arg[2]
-    end
-    
+    assert( #arg == 1 or #arg == 2, "Usage: importURL([tabname,] url)" )
+
+    local url, tabname = arg[2] or arg[1], arg[2] and arg[1]
+
     if not tabname then
-        tabname = url
-        local n = #tabname - tabname:reverse():find("/", 1) + 1
-        tabname = tabname:sub(n+1, #tabname)
+        tabname = url:sub(#url - url:reverse():find("/", 1) + 2, #url)
         tabname = tabname:sub(1, tabname:find("%.", 1) - 1)
     end
 
-    local callback = function(data, status, headers)
+    http.request(url, function(data, status, headers)
         if status == 200 then
             saveProjectTab(tabname, data)
             print("Tab '"..tabname.."' created")
         else
             print("Failed to download '"..url.."'")
         end
-    end
-    
-    http.request(url, callback)    
+    end)
 end
